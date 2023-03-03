d36ac5f0fd59770ff15ce6d7c7960f37e72311e3
barretodf <edsb1981@gmail.com>
2023-03-02 23:49:53 -0300

    Adicionando / Alterando dados do objeto

---
+++ alterar.js
@@ -0,0 +1,21 @@
+//Adicionando e alterando 
+
+const pessoa = {
+    nome: "Luma",
+    profissao: "Engenheira",
+  };
+
+  console.log(pessoa.nome);
+
+  //Acrescentando dados
+
+  pessoa.telefone = "61 3376 3964";
+  console.log(pessoa.telefone);
+
+  //Alterando dados
+
+  pessoa.nome = "Luma Silva";
+
+  console.log(pessoa);
+
+//Foi adicionado o telefone e o nome alterado
