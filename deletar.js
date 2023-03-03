c12f740da4038695150cf26f83bd94aee3a8a70c
barretodf <edsb1981@gmail.com>
2023-03-03 00:05:52 -0300

    Deletando a notação de colchetes

--- deletar.js
+++ deletar.js
@@ -15,8 +15,18 @@ const objPersonagem = {
 //console.log(objPersonagem)
 
    //Se quisermos, remover a propriedade aliado, podemos utilizar o operador delete:
-
+/*
    delete objPersonagem.aliado
 
 console.log(objPersonagem.aliado) //vai aparecer undefined
- 
+ */
+
+delete objPersonagem.aliado
+delete objPersonagem["status"]
+
+//Veja que o delete remove do objeto o valor da propriedade, assim como a chave.
+
+console.log(objPersonagem.aliado) //undefined
+console.log(objPersonagem.status) //undefined
+
+console.log(objPersonagem);
