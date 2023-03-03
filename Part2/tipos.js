b6738a0c541ed071845509f478e0426356186d9c
barretodf <edsb1981@gmail.com>
2023-03-03 20:41:37 -0300

    Tipos de dados e valores - criando array dentro da lista

---
+++ Part2/tipos.js
@@ -0,0 +1,10 @@
+//Tipos de dados e valores
+
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+  };
+
+  console.log(cliente);
