35556b64b41edbc58e4cb23fae120e0c3f1ffb36
barretodf <edsb1981@gmail.com>
2023-03-02 22:23:31 -0300

    Introdução a objetos JavaScript

---
+++ objeto.js
@@ -0,0 +1,10 @@
+//Objetos são estruturas que possuem chave e valor
+// em que a chave é o nome da propriedade e o valor é o que está armazenado na propriedade.
+//O primeiro nome é a chave e o segundo o valor ex:
+
+const objetoPessoa = {
+    nome: "Pedro",
+    idade: 20,
+    altura: 1.80,
+};
+console.log(objetoPessoa);
