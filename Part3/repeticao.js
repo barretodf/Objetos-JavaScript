a9ca289c04f0877dc71f2a7c0cc3a4af4d13a521
barretodf <edsb1981@gmail.com>
2023-03-09 00:22:55 -0300

    Como iterar dentro de um objeto 1

---
+++ Part3/repeticao.js
@@ -0,0 +1,25 @@
+//Como iterar dentro de um objeto
+
+
+const cliente = {
+    nome: "Joao",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["1155555550", "1144444440"],
+  };
+  
+  cliente.enderecos = [
+    {
+      rua: "R. Joseph Climber",
+      numero: 1337,
+      apartamento: true,
+      complemento: "ap 934",
+    },
+  ];
+  
+ 
+  for (let chave in cliente) {
+    console.log(chave);
+}
+
+//Aqui ele retorna as chaves 
